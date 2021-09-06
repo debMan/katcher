2377ef0420cb470a6206682192d00bef3b94574f
MohamadAli <mrht74@yahoo.com>
2021-09-06 17:11:06 +0430

    Prepared prometheus listener

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
-import random
-import time
-
 import prometheus_client as prom
-from easycli import Root, Argument
 
-#req_summary = prom.Summary('req_example', 'Time spent processing a request')
-#
-#
-#@req_summary.time()
-#def process_request(t):
-#   time.sleep(t)
+from broker import KafkaHandler
+from config import Config
 
-if __name__ == '__main__':
+header_field = Config().header_field
+service = Config().service
+counter_name = Config().counter_name
 
-   counter = prom.Counter('counter', 'This is my counter')
-   gauge = prom.Gauge('gauge', 'This is my gauge')
-   histogram = prom.Histogram('histogram', 'This is my histogram')
-   summary = prom.Summary('summary', 'This is my summary')
-   prom.start_http_server(8080)
+def export_header_field_value(headers, header_field=header_field):
+    value = None
+    while value is None:
+        item = headers.pop()
+        if item[0] == header_field:
+            value = item[1]
+    return value
 
-   while True:
-       counter.inc(random.random())
-       gauge.set(random.random() * 15 - 5)
-       histogram.observe(random.random() * 10)
-       summary.observe(random.random() * 10)
-       #process_request(random.random() * 5)
 
-       time.sleep(1)
+if __name__ == "__main__":
+    counter = prom.Counter(
+        counter_name,
+        "The {}'s {} counter".format(service, header_field),
+        [header_field, 'id']
+    )
+    prom.start_http_server(8080)
+    consumer = KafkaHandler()
+    for message in consumer.consume_loop():
+        api_index_id = None
+        try:
+            headers = message.headers()
+            id = export_header_field_value(headers, header_field)
+            counter.label(header_field, id).inc()
+            consumer.try_commit()
+        except Exception as e:
+            print("Unhandled error ", e)
--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-from os import path
+from os import path, getenv
 import yaml
 
 
 class Config:
 
     """
-    A simple handler for configurations. The methods of this are used to handle
-    environment variables which are configured for database and etc and some
-    properties used to configure kafka client
+    A simple handler for kafka configurations. The methods of this are used to
+    handle config properties used to configure kafka client
     """
 
-    def __init__(self, topics, config_file="config.yml"):
+    def __init__(self, config_file="config.yml"):
         """
         :type topics: list
         :type config_file: str
@@ -20,14 +19,17 @@ class Config:
             Containing the file path to config file
         """
 
-        self.topics = topics
         self.config_file = getenv("CONFIG_ADDRESS", default=config_file)
 
         # The configuration is set in a yml file.
         dirname = path.dirname(path.abspath(__file__))
         with open(str(dirname + "/" + config_file), "r") as ymlfile:
             config = yaml.safe_load(ymlfile)
-        self.topics = config['topics']
-        self.topics = config['consumer']
-
-        # All of the configuration is set in the conf attribute as dict
+        self.topics = config["topics"]
+        self.consumer = config["consumer"]
+        self.address = config["address"]
+        self.timeout = config["timeout"]
+        self.offset_reset = config["offset_reset"]
+        self.header_field = config["header_field"]
+        self.service = config["service"]
+        self.counter_name = config["counter_name"]
