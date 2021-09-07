2eb7b60ffbca1873b2257966e28b22c8087488f0
MohamadAli <mrht74@yahoo.com>
2021-09-07 15:31:03 +0430

    Hangled some exceptions

--- app.py
+++ app.py
@@ -21,21 +21,38 @@ exposed_port = Config().port
 
 def export_header_field_value(headers, header_field=header_field):
     value = None
-    for item is header:
-        if item[0] == header_field:
-            value = item[1]
-            break
+    try:
+        for item in headers:
+            if item[0] == header_field:
+                value = item[1]
+                break
+    except Exception as e:
+        print(
+            "ERROR: Error in looping on headers. Ensure topics are currect",
+            e
+        )
     return value
 
 
 if __name__ == "__main__":
     prom.start_http_server(exposed_port)
     consumer = KafkaHandler()
+    print("Daemon started successfully on port {} ...".format(exposed_port))
     for message in consumer.consume_loop():
         try:
             headers = message.headers()
-            api_index_id = export_header_field_value(headers, header_field)
-            consumer.try_commit()
-            counter.labels(int(api_index_id)).inc()
+            if headers != None:
+                api_index_id = export_header_field_value(
+                    headers,
+                    header_field
+                )
+                try:
+                    api_index_id = int(api_index_id)
+                except TypeError as e:
+                    print("ERROR: invalid header value")
+                    continue
+                finally:
+                    consumer.try_commit()
+                counter.labels((api_index_id)).inc()
         except Exception as e:
-            print("Unhandled error ", e)
+            print("ERROR: ", e)
